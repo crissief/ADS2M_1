bf2a285ae0c56a4cc11cb54e6867f72cbf1aefeb
Crissie Farias <crissifarias@hotmail.com>
2014-03-29 14:10:07 -0300

    Implementado o metodo menu

---
+++ SistemaBancario/src/com/senac/mvc/app/BancoApp.java
@@ -0,0 +1,14 @@
+package com.senac.mvc.app;
+import com.senac.mvc.controller.*;
+import com.senac.mvc.excecao.SaldoInsuficienteException;
+import com.senac.mvc.model.*;
+
+public class BancoApp {
+	public static void main(String[] args) {
+		BancoController bc = new BancoController();
+		
+		
+
+	}
+
+}
--- SistemaBancario/src/com/senac/mvc/controller/BancoController.java
+++ SistemaBancario/src/com/senac/mvc/controller/BancoController.java
@@ -1,5 +1,31 @@
 package com.senac.mvc.controller;
 
-public class BancoController {
+import com.senac.mvc.model.Cliente;
+import com.senac.mvc.model.Conta;
+import com.senac.mvc.model.Especial;
+import com.senac.mvc.model.Investimento;
+import java.util.*;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+public class BancoController {
+	Cliente cli;
+	Conta conta;
+	Especial contEsp;
+	Investimento contInv;
+	Scanner ler = new Scanner(System.in);
+	int menu = 0;
+	
+	// metodo que mostra o menu de opções ao usuário
+	public void menu(){
+		System.out.println("Selecione uma das opções: " +
+				"\n1- Criar Cliente" +
+				"\n2- Criar Conta" +
+				"\n3- Criar Conta Especial" +
+				"\n4- Criar Conta Poupança");
+		menu = ler.nextInt();
+	}
+	
 }
+
+
